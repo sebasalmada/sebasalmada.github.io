39b288f32b09b135e7e43ab0eb927a42d64acd50
sebasalmada <salmada86@gmail.com>
2020-04-17 12:14:17 -0300

    Updates

--- precache-manifest.aa78ee3e123f41ce3775fc50f2ac15bc.js
+++ precache-manifest.aa78ee3e123f41ce3775fc50f2ac15bc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e57857fbeb2e3d84c9460fb50d3f915",
+    "revision": "994209feb07863c0f3a795c63761a445",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "70ed18cf19700b7ea954",
+    "revision": "01ae388568b093cae3c3",
     "url": "/static/css/main.745575f9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.29a9f8e2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "70ed18cf19700b7ea954",
-    "url": "/static/js/main.5bd09b1f.chunk.js"
+    "revision": "01ae388568b093cae3c3",
+    "url": "/static/js/main.e095d8e8.chunk.js"
   },
   {
     "revision": "95bd842dd82388d163ee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b92c8c570a1f50a14cb3f6ccd7c5e57.js"
+  "/precache-manifest.aa78ee3e123f41ce3775fc50f2ac15bc.js"
 );
 
 self.addEventListener('message', (event) => {
